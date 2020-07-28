b09bab4be36ccd663bd575940825c41689360c58
AhmetEsad <38183175+AhmetEsad@users.noreply.github.com>
2020-07-28 21:09:56 +0300

    Gereksiz alt çizgileri sildim

--- sakla.js
+++ sakla.js
@@ -7,26 +7,26 @@ exports.okut = function (dosya) {
         return "HATA: dosya boş veya bozuk: tüm satırların X=Y şeklinde olması gerekir.";
     }
 }
-exports.ekle = function (_dosya, _json) {
-    for (o in Object.entries(_json)[0]) {
-        var okunan = fs.appendFileSync(_dosya, fs.readFileSync(_dosya, "utf8") !== "" ? "\n" + Object.entries(_json).map(d => d[0] + '=' + d[1]).join('\n') : Object.entries(_json).map(d => d[0] + '=' + d[1]).join('\n'));
-        return exports.okut(_dosya);
+exports.ekle = function (dosya, json) {
+    for (o in Object.entries(json)[0]) {
+        var okunan = fs.appendFileSync(dosya, fs.readFileSync(dosya, "utf8") !== "" ? "\n" + Object.entries(json).map(d => d[0] + '=' + d[1]).join('\n') : Object.entries(json).map(d => d[0] + '=' + d[1]).join('\n'));
+        return exports.okut(dosya);
     }
 }
-exports.sil = function (__dosya, __isim) {
-    var okunanDosya = fs.readFileSync(__dosya, "utf8");
-    if (okunanDosya.split("\n").filter(satır => /(.*)=(.*)/.exec(satır)[1] == __isim)) {
-        var _e = exports.okut(__dosya);
-        var _y = delete _e[__isim];
-        exports.üzerineYaz(__dosya, _e);
-        return exports.okut(__dosya);
+exports.sil = function (dosya, isim) {
+    var okunanDosya = fs.readFileSync(dosya, "utf8");
+    if (okunanDosya.split("\n").filter(satır => /(.*)=(.*)/.exec(satır)[1] == isim)) {
+        var _e = exports.okut(dosya);
+        var _y = delete _e[isim];
+        exports.üzerineYaz(dosya, _e);
+        return exports.okut(dosya);
     } else {
         return "HATA: değişken yok: yalnızca daha önceden tanımlanmış değişkenler silinebilir."
     }
 }
-exports.üzerineYaz = function (___dosya, ___json) {
-    for (o in Object.entries(___json)[0]) {
-        var okunan = fs.writeFileSync(___dosya, Object.entries(___json).map(d => d[0][o] + '=' + d[1][o]).join('\n'));
-        return exports.okut(___dosya);
+exports.üzerineYaz = function (dosya, json) {
+    for (o in Object.entries(json)[0]) {
+        var okunan = fs.writeFileSync(dosya, Object.entries(json).map(d => d[0][o] + '=' + d[1][o]).join('\n'));
+        return exports.okut(dosya);
     }
 }
